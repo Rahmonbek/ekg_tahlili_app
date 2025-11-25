4f5804d3be320161f1dd9817c1b9123c71a348ca
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2025-11-25 18:20:29 +0500

    fixec

--- frontend/src/host/EkgService.js
+++ frontend/src/host/EkgService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const analyzeEkgFile = async (formData) => {
-  const res = await axios.post("http://localhost:8000/api/analyze", formData, {
+  const res = await axios.post("http://127.0.0.1:8000/api/analyze", formData, {
     headers: { "Content-Type": "multipart/form-data" },
   });
   return res.data;
