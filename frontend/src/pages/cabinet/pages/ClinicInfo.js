91840d398104499ad78d69b2e49c1b4e990b58cd
Ibrohim39 <ibrohimismoiloff07@gmail.com>
2025-12-23 23:26:03 +0500

    main

--- frontend/src/pages/cabinet/pages/ClinicInfo.js
+++ frontend/src/pages/cabinet/pages/ClinicInfo.js
@@ -188,10 +188,9 @@ const onFinishFinish = async (values) => {
     if (licenseFile) {
       formData.append("LicenseFile", licenseFile);
     }
-     getClinicData()
-    await send_clinic_detail(formData);
-    successAlert(t("data_saved"))
-    getClinicData()
+    await send_clinic_detail(formData); 
+    successAlert(t("data_saved"));
+    getClinicData();  
 
   } catch (error) {
     console.error(error);
