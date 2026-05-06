49c907f4df67cb66909cc32f93fef0ea7bb1d789
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2026-05-06 17:34:09 +0500

    fixed

--- frontend/src/tools/routes.js
+++ frontend/src/tools/routes.js
@@ -5,71 +5,71 @@ import { MdOutlineMedicalInformation } from "react-icons/md";
 import { GiTestTubes } from "react-icons/gi";
 import { RiPulseLine } from 'react-icons/ri';
 import { GiMicroscope } from 'react-icons/gi';
-export const routers=[
+export const routers = [
     {
-        path:'/doctor',
-        icon:<FaUserDoctor />,
-        title:"staffs",
-        tools:"doctor",
-        role_id:[2, 3],
+        path: '/doctor',
+        icon: <FaUserDoctor />,
+        title: "staffs",
+        tools: "doctor",
+        role_id: [2, 3],
         unread_key: null,
         requires_active: false   // Admin doim kira oladi
     },
 
-     {
-        path:'/ecg-analyses',
-        icon:<FaHeartbeat />,
-        title:"analyse_ecg",
-        tools:"ecg-analyses",
-        role_id:[],
+    {
+        path: '/ecg-analyses',
+        icon: <FaHeartbeat />,
+        title: "analyse_ecg",
+        tools: "ecg-analyses",
+        role_id: [],
         unread_key: 'ecg_unread',
         requires_active: true
     },
     {
-        path:'/holter-analyses',
-        icon:<RiPulseLine />,
-        title:"analyse_holter",
-        tools:"holter-analyses",
-        role_id:[],
+        path: '/holter-analyses',
+        icon: <RiPulseLine />,
+        title: "analyse_holter",
+        tools: "holter-analyses",
+        role_id: [],
         unread_key: 'holter_unread',
         requires_active: true
     },
     {
-        path:'/smad-analyses',
-        icon:<FaChartLine />,
-        title:"analyse_smad",
-        tools:"smad-analyses",
-        role_id:[],
+        path: '/smad-analyses',
+        icon: <FaChartLine />,
+        title: "analyse_smad",
+        tools: "smad-analyses",
+        role_id: [],
         unread_key: 'smad_unread',
         requires_active: true
     },
-     {
-        path:'/lab-analyses',
-        icon:<GiTestTubes />,
-        title:"analyse_lab",
-        tools:"lab-analyses",
-        role_id:[],
+    {
+        path: '/lab-analyses',
+        icon: <GiTestTubes />,
+        title: "analyse_lab",
+        tools: "lab-analyses",
+        role_id: [],
         unread_key: 'lab_unread',
         requires_active: true
     },
-     {
-        path:'/patient-diagnoses',
-        icon:< MdOutlineMedicalInformation />,
-        title:"patient_diagnostics",
-        tools:"patient-diagnoses",
-        role_id:[],
+    {
+        path: '/patient-diagnoses',
+        icon: < MdOutlineMedicalInformation />,
+        title: "patient_diagnostics",
+        tools: "patient-diagnoses",
+        role_id: [],
         unread_key: 'diagnoses_unread',
         requires_active: true
     },
-    {
-        path:'/parasitology-analyses',
-        icon:<GiMicroscope />,
-        title:"parasitology_analyse",
-        tools:"parasitology-analyses",
-        role_id:[],
-        unread_key: null,
-        requires_active: true
-    },
+    // {
+    //     path:'/parasitology-analyses',
+    //     icon:<GiMicroscope />,
+    //     title:"parasitology_analyse",
+    //     tools:"parasitology-analyses",
+    //     role_id:[],
+    //     unread_key: null,
+    //     requires_active: true
+    // },
     //  {
     //     path:'/patcients',
     //     icon:<FaPeopleGroup />,
@@ -87,11 +87,11 @@ export const routers=[
     //     requires_active: false
     // },
     {
-        path:'/settings',
-        icon:<IoMdSettings />,
-        title:"organization_info",
-        tools:"settings",
-        role_id:[2, 3],
+        path: '/settings',
+        icon: <IoMdSettings />,
+        title: "organization_info",
+        tools: "settings",
+        role_id: [2, 3],
         unread_key: null,
         requires_active: false   // Admin doim kira oladi
     },
